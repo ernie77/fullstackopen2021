1f91b50ca257137faea92f7768ed0749267c4049
ernie77 <henri.maki@gmail.com>
2019-11-26 15:27:48 +0200

    step3

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -45,8 +45,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises3} />
-      <Total exercises1={part1.exercise} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
