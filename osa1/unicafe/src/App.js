0a97a45b2fa2d36fd49a27bd389b358ac78775b7
ernie77 <henri.maki@gmail.com>
2021-10-21 18:09:17 +0300

    step4

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,16 +6,28 @@ const Button = ({ handleClick, text }) => (
 	</button>
 )
 
-const Statistics = ({ good, neutral, bad }) => (	
-	<>
-		Good {good} <br />
-		Neutral {neutral} <br />
-		Bad {bad} <br />
-		All {good + neutral + bad} <br />
-		Average {(good - bad)/(good + neutral + bad)} <br />
-		Positive {(100*good)/(good + neutral + bad)} % <br />
-	</>
-)
+const Statistics = ({ good, neutral, bad }) => {
+	const all = good + neutral + bad
+	
+	if ( all === 0 ) {
+		return (
+			<>			
+			No feedback given
+			</>
+		)
+	}
+	
+	return (
+		<>
+			Good {good} <br />
+			Neutral {neutral} <br />
+			Bad {bad} <br />
+			All {all} <br />
+			Average {(good - bad)/(all)} <br />
+			Positive {(100*good)/(all)} % <br />
+		</>
+	)
+}
 
 
 const App = () => {
@@ -23,18 +35,19 @@ const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
-  
+	  
 	const handleGoodClick = () => {
 		setGood(good + 1)
-	}
+		}
 
 	const handleNeutralClick = () => {
 	 	setNeutral(neutral + 1)
-	}
+		}
   
 	const handleBadClick = () => {
 		setBad(bad + 1)
-	}
+		}
+	
 	return (
 		<div>
 			<h1>Give feedback</h1>
